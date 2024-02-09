8f704e2b0d4df01c9210e5fe26d0a951e9e7e2d7
Rafiul-Islam <rafi.cse.ru@gmail.com>
2024-02-09 23:44:54 +0600

    feature - Product card design done

--- src/compoents/Product.tsx
+++ src/compoents/Product.tsx
@@ -2,9 +2,23 @@ import ProductModel from "../models/Product.ts";
 import {FaRegStar} from "react-icons/fa";
 
 const Product = ({product: {name, image, price, fastDelivery}}: { product: ProductModel }) => {
+
+    const generateRandomDay = () => {
+        const days = parseInt(Math.random() * 5);
+        return days > 1 ? days : generateRandomDay();
+    }
+
     return (
         <div className="card product-card">
-            <img width='100%' height='200' style={{objectFit: "cover"}} src={image} loading='lazy' className="card-img-top" alt="A random Image"/>
+            <img
+                width='100%'
+                height='200'
+                style={{objectFit: "cover"}}
+                src={image}
+                loading='lazy'
+                className="card-img-top"
+                alt="A random Image"
+            />
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <strong className="card-text">
@@ -12,15 +26,15 @@ const Product = ({product: {name, image, price, fastDelivery}}: { product: Produ
                 </strong>
                 <br/>
                 <strong className="card-text">
-                    {fastDelivery ? 'Fast Delivery' : "4 days delivery"}
+                    {fastDelivery ? 'Fast Delivery' : `${generateRandomDay()} days delivery`}
                 </strong>
                 <br/>
                 <div className='mb-2'>
-                    <FaRegStar className='ms-1' />
-                    <FaRegStar className='ms-1' />
-                    <FaRegStar className='ms-1' />
-                    <FaRegStar className='ms-1' />
-                    <FaRegStar className='ms-1' />
+                    <FaRegStar className='ms-1'/>
+                    <FaRegStar className='ms-1'/>
+                    <FaRegStar className='ms-1'/>
+                    <FaRegStar className='ms-1'/>
+                    <FaRegStar className='ms-1'/>
                 </div>
                 <button type='button' className="btn btn-primary add-to-cart-btn">Add To Cart</button>
             </div>
